7e62bf738e22dfe655c25b87470027e1793338a5
Irina Alyabeva <irinpingviiin@yandex.ru>
2023-01-05 01:49:05 +0300

    Практическая работа 13
    Создание контроллеров и роутов для пользователей

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const router = require('./routes/users');
 
 const {PORT = 3000} = process.env;
 const app = express();
+
+app.use('/users', router);
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
