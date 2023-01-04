271f0fada4b929d857f514d82925cf956a0c5df7
Irina Alyabeva <irinpingviiin@yandex.ru>
2023-01-04 03:48:58 +0300

    Практическая работа 13
    Подключение к БД, создание схем user, card

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const {PORT = 3000} = process.env;
 const app = express();
 
-app.listen(PORT, () => {
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
-});
+app.listen(PORT);
