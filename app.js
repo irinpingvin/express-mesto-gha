5aa3d4a895d83db0cd6732125c72ae9151cf756b
Irina Alyabeva <irinpingviiin@yandex.ru>
2023-01-05 22:29:21 +0300

    Практическая работа 13
    Временное решение авторизации

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '63b60baf713e73f8cd8fcab0'
+  };
+
+  next();
+});
+
 app.use('/', router);
 
 app.listen(PORT);
